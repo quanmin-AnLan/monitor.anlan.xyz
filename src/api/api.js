36fb79de55cbcede8805b054118d8fb365939aa3
wnw <ningweiwang218830@sohu-inc.com>
2022-08-15 14:49:20 +0800

    change: api

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,9 @@
 import Http from './service'
 
 const apis = {
-    getReportList: (params) => Http.get('/getReportList', params),
-    getSpmList: (params) => Http.get('/getSpmList', params),
-    addSpmList: (params) => Http.post('/addSpmList', params)
+    getReportList: (params) => Http.get('/report/getList', params),
+    getSpmList: (params) => Http.get('/report/spm/getList', params),
+    addSpmList: (params) => Http.post('/report/spm/add', params)
 }
 
 export default apis
